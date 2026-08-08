1f47be9dba7a94427aa325d393daab4dabf5dc2c
Najam <najammaudarbaccus@gmail.com>
2026-08-08 00:15:10 -0400

    Project skeleton, Database connection and NASA Configurations

---
+++ src/test/java/com/neonajam/neotracker/NeoTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neonajam.neotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
